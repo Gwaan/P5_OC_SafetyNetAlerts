6d7e22cc4e4113b2898c50961fcb0cd03a4c6f93
Gwenaël Boisnard <boisnard.gwenael@gmail.com>
2021-03-04 15:36:13 +0100

    Added tests for FloodControllerIT class

---
+++ src/test/java/com/safetynet/alerts/integration/FloodControllerIT.java
@@ -0,0 +1,28 @@
+package com.safetynet.alerts.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class FloodControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void should_Return_A_FloodDTOList_Covered_By_Station_Number() throws Exception {
+
+        mockMvc
+                .perform(get("/flood/stations?stations=1,2"))
+                .andExpect(status().isOk());
+    }
+
+
+}
